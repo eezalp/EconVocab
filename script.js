f1730b8be3d019694870ecf2fea545cf272a5b92
eezalp <112824152+eezalp@users.noreply.github.com>
2025-03-02 18:27:28 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function OnLoad(){
   vocab = "Error retreiving json"
   fetch("Vocab.json")
     .then(res => res.json())
-    .then(res => {vocab = res})
+    .then(res => vocab = res)
     .catch(error => console.error("Error retreiving vocab", error));
   if(vocab){
     document.getElementById("text").innerHTML = vocab;
